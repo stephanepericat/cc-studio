b9c4784dad09af00d904b16b4a9f6298a45ba1cb
Stephane P. Pericat <shortwavaudio@gmail.com>
2023-05-27 17:13:29 -0500

    fix language order

--- config/languages.js
+++ config/languages.js
@@ -1,8 +1,8 @@
 export const supportedLanguages = [
   { id: 'en', title: 'English', isDefault: true },
   { id: 'fr', title: 'French' },
-  { id: 'es', title: 'Spanish' },
   { id: 'pt', title: 'Portuguese' },
+  { id: 'es', title: 'Spanish' },
 ];
 
 export const baseLanguage = supportedLanguages.find(l => l.isDefault);
