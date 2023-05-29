1c247458005204d2f24797091254a60db70a6843
Stephane P. Pericat <shortwavaudio@gmail.com>
2023-05-28 20:37:32 -0500

    add slug to categories

--- schemas/communityCategory.js
+++ schemas/communityCategory.js
@@ -11,6 +11,15 @@ export default {
       title: 'Name',
       validation: Rule => Rule.required(),
     },
+    {
+      name: 'uri',
+      type: 'slug',
+      title: 'URI',
+      options: {
+        source: `name.${baseLanguage.id}`,
+      },
+      validation: Rule => Rule.required(),
+    },
   ],
   preview: {
     select: {
