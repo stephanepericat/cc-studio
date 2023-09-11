a935c9651e3c3be7e85d662bcd1def31426b13ef
Stephane P. Pericat <shortwavaudio@gmail.com>
2023-09-11 12:57:22 -0500

    different envs

--- sanity.config.ts
+++ sanity.config.ts
@@ -4,21 +4,42 @@ import {deskTool} from 'sanity/desk'
 import {media} from 'sanity-plugin-media'
 import {schemaTypes} from './schemas'
 
-export default defineConfig({
-  name: 'default',
-  title: 'Covid Conscious CMS',
-
-  projectId: 'yt0dcu6v',
-  dataset: 'production',
-
-  plugins: [
-    deskTool(),
-    media(),
-    visionTool(),
-  ],
-
-  schema: {
-    types: schemaTypes,
+export default defineConfig([
+  {
+    name: 'production-workspace',
+    title: 'That Covid Life CMS - Production',
+    basePath: '/production',
+  
+    projectId: 'yt0dcu6v',
+    dataset: 'production',
+  
+    plugins: [
+      deskTool(),
+      media(),
+      visionTool(),
+    ],
+  
+    schema: {
+      types: schemaTypes,
+    },
   },
-})
+  {
+    name: 'preview-workspace',
+    title: 'That Covid Life CMS - Preview',
+    basePath: '/preview',
+  
+    projectId: 'yt0dcu6v',
+    dataset: 'development',
+  
+    plugins: [
+      deskTool(),
+      media(),
+      visionTool(),
+    ],
+  
+    schema: {
+      types: schemaTypes,
+    },
+  }
+])
 
