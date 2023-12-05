b2550b4fe9ad14f7d6f8715e60697d58421a6e45
Stephane P. Pericat <shortwavaudio@gmail.com>
2023-12-05 14:55:32 -0600

    library

---
+++ schemas/scientificLibrary.js
@@ -0,0 +1,61 @@
+import { baseLanguage } from "../config/languages"
+
+export default {
+  name: 'scientificLibrary',
+  type: 'document',
+	title: 'Scientific Library',
+  fields: [
+    {
+      name: 'title',
+      type: 'localeString',
+      title: 'Title',
+      validation: Rule => Rule.required(),
+    },
+    {
+      name: 'description',
+      type: 'localeText',
+      title: 'Description',
+    },
+    {
+      name: 'author',
+      type: 'reference',
+      title: 'Author',
+      to: [{type: 'author'}],
+      validation: Rule => Rule.required(),
+    },
+    {
+      name: 'category',
+      type: 'reference',
+      title: 'Category',
+      to: [{type: 'scientificLibraryCategory'}],
+      validation: Rule => Rule.required(),
+    },
+    {
+      name: 'url',
+      type: 'url',
+      title: 'URL',
+      validation: Rule => Rule.required(),
+    },
+    {
+      name: 'source',
+      type: 'string',
+      title: 'Source',
+      validation: Rule => Rule.required(),
+    },
+    {
+      name: 'visual',
+      type: 'image',
+      title: 'Thumbnail',
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+  preview: {
+    select: {
+      media: 'visual',
+      subtitle: `source`,
+      title: `title.${baseLanguage.id}`
+    }
+  },
+}
