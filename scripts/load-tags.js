aecc6133a7518e0c09ab837430bbdb08a12f2a76
Stephane P. Pericat <shortwavaudio@gmail.com>
2024-01-04 19:24:01 -0600

    script to load tags

---
+++ scripts/load-tags.js
@@ -0,0 +1,43 @@
+import dotenv from 'dotenv'
+
+import { createClient } from '@sanity/client'
+import _ from 'lodash'
+
+dotenv.config()
+
+const { SANITY_DATASET, SANITY_PROJECTID, SANITY_TOKEN } = process.env
+// console.log(SANITY_DATASET, SANITY_PROJECTID)
+
+const client = createClient({
+  projectId: SANITY_PROJECTID,
+  dataset: SANITY_DATASET,
+  useCdn: true, // set to `false` to bypass the edge cache
+  apiVersion: '2023-09-04',
+  token: SANITY_TOKEN,
+})
+
+// console.log('client', client)
+const categories = await client.fetch('*[_type in ["communityCategory", "educationCategory", "linkCategory", "productCategory", "resourceCategory", "scientificLibraryCategory"]]')
+const uniqueCategories = _.uniqBy(categories, 'name.en').map(({ name, uri }) => {
+  delete name._type
+  return {
+    name,
+    uri: uri.current,
+  }
+})
+
+const loadTag = async (name, uri) => {
+  const doc = {
+    _type: 'tag',
+    name,
+    uri,
+  }
+
+  return await client.create(doc)
+}
+
+console.log('categories', uniqueCategories)
+
+uniqueCategories.forEach(async ({ name, uri }) => {
+  await loadTag(name, uri)
+})
