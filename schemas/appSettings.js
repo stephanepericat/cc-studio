55f255d8e82a1cf4df49a2a05587c8350d941b3d
Stephane P. Pericat <shortwavaudio@gmail.com>
2024-01-21 13:11:53 -0600

    app settings content type

---
+++ schemas/appSettings.js
@@ -0,0 +1,53 @@
+import { baseLanguage } from "../config/languages"
+
+export default {
+  name: 'appSettings',
+  type: 'document',
+	title: 'App Settings',
+  fields: [
+    {
+      name: 'key',
+      type: 'string',
+      title: 'key',
+      validation: Rule => Rule.required(),
+    },
+    {
+      name: 'type',
+      type: 'string',
+      title: 'Type',
+      options: {
+        list: [
+          { title: 'String', value: 'string'},
+          { title: 'Boolean', value: 'boolean'},
+          { title: 'Number', value: 'number'},
+        ],
+        layout: 'dropdown'
+      },
+      validation: Rule => Rule.required(),
+    },
+    {
+      name: 'valueString',
+      type: 'string',
+      title: 'Value',
+      hidden: ({ document }) => document.type !== 'string',
+    },
+    {
+      name: 'valueBoolean',
+      type: 'boolean',
+      title: 'Enabled',
+      hidden: ({ document }) => document.type !== 'boolean',
+    },
+    {
+      name: 'valueNumber',
+      type: 'number',
+      title: 'Value',
+      hidden: ({ document }) => document.type !== 'number',
+    },
+  ],
+  preview: {
+    select: {
+      title: 'key',
+      subtitle: 'value'
+    }
+  },
+}
